732854bdb45296d4276d80b91a89140fe14f3c24
pravk37 <bogipillipraveenkumar@gmail.com>
2024-11-25 10:13:43 +0530

    Docker Commands

---
+++ dockercommand.sh
@@ -0,0 +1,1 @@
+docker run -it --name data-copier --rm --network data-copier-nw -v C:\Users\bogip\Project\Internal\data\retail_db_json:/retail_db_json -e BASE_DIR=/retail_db_json -e DB_HOST=f8ee84bbc637 -e DB_PORT=5432 -e DB_NAME=retail_db -e DB_USER=retail_user -e DB_PASS=itversity data-copier python /data-copier/app/app.py departments,categories
